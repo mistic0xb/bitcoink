98049a0aa4f2f2d2bc7a299336d38e7b33669398
Mist <mistic0xb@gmail.com>
2025-09-18 21:45:06 +0530

    feat: point adding method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.mist
 
+import org.mist.ecc.Point
+
 fun main() {
-    println("Building bitcoink-lib to learn about bitcoin")
+    val p1 = Point(2.0, 5.0, 5.0, 7.0)
+    val p2 = Point(-1.0, -1.0, 5.0, 7.0)
+    println(p1.add(p2))
 }
--- src/main/kotlin/ecc/Point.kt
+++ src/main/kotlin/ecc/Point.kt
@@ -2,12 +2,84 @@ package org.mist.ecc
 
 import kotlin.math.pow
 
-data class Point(val x:Double,val y:Double,val a:Double,val b:Double) {
+/*
+[secp256k1]
+y^2 = x^3 + ax + b
+y^2 = x^3 + 7
+here, a = 0, b = 7
+*/
+data class Point(val x: Double?, val y: Double?, val a: Double, val b: Double) {
     init {
-        if(y.pow(2) != x.pow(3) + a*x + b){
-           throw IllegalArgumentException(
-               "($x,$y) is not on the curve"
-           )
+        if (x != null && y != null) {
+            if (y.pow(2) != x.pow(3) + a * x + b) {
+                throw IllegalArgumentException(
+                    "($x,$y) is not on the curve"
+                )
+            }
         }
+    }
+
+    fun add(other: Point): Point {
+        if (a != other.a || b != other.b) {
+            throw IllegalArgumentException(
+                "Points ($x,$y) are not on the same curve"
+            )
+        }
+        // handle infinity
+        if (x == null) {
+            return other
+        }
+        if (other.x == null) {
+            return this
+        }
+
+        // A + (-A) = I (tangent is vertical)
+        if (x == other.x && y != other.y) {
+            return Point(
+                x = null,
+                y = null,
+                a = a,
+                b = b
+            )
+        }
+
+        // x1 != x2
+        if (x != other.x ) {
+            val slope = (other.y!! - y!!) / (other.x - x)
+            val x3 = slope.pow(2) - other.x - x
+            val y3 = slope * (x - x3) - y
+            return Point(
+                x = x3,
+                y = y3,
+                a = a,
+                b = b
+            )
+        }
+
+        // P1 == P2
+        if (this == other) {
+            val slope = (3 * x.pow(2) + a) / (2 * y!!)
+            val x3 = slope.pow(2) - 2 * x
+            val y3 = slope * (x - x3) - y
+            return Point(
+                x = x3,
+                y = y3,
+                a = a,
+                b = b
+            )
+        }
+
+        // P1 == P2 && y == 0
+        if (this == other && y == 0.0) {
+            return Point(
+                x = null,
+                y = null,
+                a = a,
+                b = b
+            )
+        }
+        throw IllegalStateException(
+            "Unhandled case in point addition for point() for:\n point($x,$y) on eq: y^2 = x^3 + ${a}x + $b"
+        )
     }
 }
