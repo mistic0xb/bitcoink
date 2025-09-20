bf04ed2a1636e78271569eace744b8c5bb5d7bd3
Mist <mistic0xb@gmail.com>
2025-09-20 09:20:23 +0530

    fix: changes methods to operator methods for easy usage in forming math formulas

--- src/main/kotlin/ecc/FieldElement.kt
+++ src/main/kotlin/ecc/FieldElement.kt
@@ -15,48 +15,57 @@ data class FieldElement(val num: Int, val prime: Int) {
         return "FieldElement_$prime($num)"
     }
 
-    fun add(other: FieldElement): FieldElement {
+    private fun mod(value: Int, modulus: Int): Int {
+        val result = value % modulus
+        if (result < 0) {
+            return result + modulus
+        } else {
+            return result
+        }
+    }
+
+    operator fun plus(other: FieldElement): FieldElement {
         if (prime != other.prime) {
             throw IllegalArgumentException(
                 "Cannot add two numbers in different Fields"
             )
         }
-        val newNum = (num + other.num) % prime
+        val newNum = mod((num + other.num), prime)
         return FieldElement(newNum, prime)
     }
 
-    fun sub(other: FieldElement): FieldElement {
+    operator fun minus(other: FieldElement): FieldElement {
         if (prime != other.prime) {
             throw IllegalArgumentException(
                 "Cannot subtract two numbers in different Fields"
             )
         }
-        val res = (num - other.num) % prime
+        val res = mod((num - other.num), prime)
         return FieldElement(res, prime)
     }
 
-    fun mul(other: FieldElement): FieldElement {
+    operator fun times(other: FieldElement): FieldElement {
         if (prime != other.prime) {
             throw IllegalArgumentException(
                 "Cannot multiply two numbers in different Fields"
             )
         }
-        val res = (num * other.num) % prime
+        val res = mod((num * other.num), prime)
         return FieldElement(res, prime)
     }
 
-    fun div(other: FieldElement): FieldElement {
+    operator fun div(other: FieldElement): FieldElement {
         if (prime != other.prime) {
             throw IllegalArgumentException(
                 "Cannot divide two numbers in different Fields"
             )
         }
-        val res = (num * (other.power(prime - 2).num)) % prime
+        val res = mod((num * (other.pow(prime - 2).num)), prime)
         return FieldElement(res, prime)
     }
 
-    fun power(exp: Int): FieldElement {
-        val n = exp % (prime - 1)
+    fun pow(exp: Int): FieldElement {
+        val n = mod(exp, (prime - 1))
         val base = BigInteger.valueOf(num.toLong())
         val mod = BigInteger.valueOf(prime.toLong())
         val positiveExpo = BigInteger.valueOf(n.toLong())
